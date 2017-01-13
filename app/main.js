91fc385b3ccab8d3aab124f52ebd19d8e896ae4a
efectotam <tam.luque@gmail.com>
2017-01-13 17:13:01 -0300

    Buscar hijos, seleccionar primer hijo

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
-class World {
+/*Ejercicio 1: Hola mundo*/
+
+/*class World {
   constructor(message, name) {
     this.message = message;
     this.name = name;
@@ -12,4 +14,27 @@ class World {
 const name = "Lemontech";
 const world = new World("Hola", name);
 
-world.print();
+world.print();*/
+
+/*Ejercicio 2: Buscar hijos. Retornar el primer elemento.*/
+
+class Selector {
+  constructor(parent) {
+    this.parent = parent;
+    this.children =$(this.parent).children()
+  }
+  print() {
+    console.log(this.children);
+  }
+}
+const selector = new Selector("span");
+selector.print();
+
+
+class FancySelector extends Selector{
+  print() {
+    console.log(this.children.first());
+  }
+}
+const fancySelector = new FancySelector('span');
+fancySelector.print();
--- public/build.js
+++ public/build.js
@@ -48,34 +48,74 @@
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	var World = function () {
-	  function World(message, name) {
-	    _classCallCheck(this, World);
+	/*Ejercicio 1: Hola mundo*/
 
+	/*class World {
+	  constructor(message, name) {
 	    this.message = message;
 	    this.name = name;
 	  }
+	 
+	  print() {
+	    $('.title').append(`${this.message} ${this.name}`);
+	  }
+	}
 
-	  _createClass(World, [{
+	const name = "Lemontech";
+	const world = new World("Hola", name);
+
+	world.print();*/
+
+	/*Ejercicio 2: Buscar hijos. Retornar el primer elemento.*/
+
+	var Selector = function () {
+	  function Selector(parent) {
+	    _classCallCheck(this, Selector);
+
+	    this.parent = parent;
+	    this.children = $(this.parent).children();
+	  }
+
+	  _createClass(Selector, [{
 	    key: "print",
 	    value: function print() {
-	      $('.title').append(this.message + " " + this.name);
+	      console.log(this.children);
 	    }
 	  }]);
 
-	  return World;
+	  return Selector;
 	}();
 
-	var name = "Lemontech";
-	var world = new World("Hola", name);
+	var selector = new Selector("span");
+	selector.print();
 
-	world.print();
+	var FancySelector = function (_Selector) {
+	  _inherits(FancySelector, _Selector);
 
-	var selector = new Selector('.class');
+	  function FancySelector() {
+	    _classCallCheck(this, FancySelector);
 
-	var fancySelector = new FancySelector('.class');
+	    return _possibleConstructorReturn(this, (FancySelector.__proto__ || Object.getPrototypeOf(FancySelector)).apply(this, arguments));
+	  }
+
+	  _createClass(FancySelector, [{
+	    key: "print",
+	    value: function print() {
+	      console.log(this.children.first());
+	    }
+	  }]);
+
+	  return FancySelector;
+	}(Selector);
+
+	var fancySelector = new FancySelector('span');
+	fancySelector.print();
 
 /***/ }
 /******/ ]);
